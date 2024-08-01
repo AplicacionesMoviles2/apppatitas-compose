5429ca6bbcd2d8263b9c1fedd666d053ceba16e9
AplicacionesMoviles2 <juankuga@Juans-MacBook-Pro.local>
2024-07-31 21:41:01 -0500

    HomeScreen menu de opciones de la aplicación terminado

--- app/src/main/java/pe/idat/apppatitas_compose/home/view/HomeScreen.kt
+++ app/src/main/java/pe/idat/apppatitas_compose/home/view/HomeScreen.kt
@@ -10,16 +10,27 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Checklist
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Pets
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,17 +38,57 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import pe.idat.apppatitas_compose.core.utils.MenuItem
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun homeScreen() {
-    Box(
-        modifier =
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val coroutineScope = rememberCoroutineScope()
+    ModalNavigationDrawer(drawerState = drawerState,
+        drawerContent = {
+            drawerContent(items = opcionesMenu(), onItemClick = {
+                item -> coroutineScope.launch {
+                    drawerState.close()
+            }})
+
+        },
+        content = {
+            Column {
+                TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.White,
+                    titleContentColor = Color.Black
+                ),
+                    title = { Text(text = "Patitas APP") },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            coroutineScope.launch {
+                                drawerState.open()
+                            }
+                        }) {
+                            Icon(Icons.Default.Menu, contentDescription = "Menu")
+                        }
+                    })
+            }
+        })
+}
+@Composable
+fun drawerContent(
+    items: List<MenuItem>,
+    onItemClick: (MenuItem) -> Unit
+){
+    Column(
         Modifier
             .fillMaxSize()
-            .background(Color.Blue)
-    ) {
-        Text(text = "Bienvenido", modifier = Modifier.align(Alignment.Center))
+            .background(Color.White)
+            .systemBarsPadding()) {
+        cabeceraMenu()
+        Spacer(modifier = Modifier.height(8.dp))
+        items.forEach{
+            item ->
+            drawerMenuItem(item, onItemClick)
+        }
     }
 }
 
