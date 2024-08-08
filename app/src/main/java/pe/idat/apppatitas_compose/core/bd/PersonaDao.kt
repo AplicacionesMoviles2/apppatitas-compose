e218ed3c9669bfdb3e4f92e50382e0e004bc4662
AplicacionesMoviles2 <juankuga@Juans-MacBook-Pro.local>
2024-08-07 21:46:59 -0500

    diseño de registro voluntario e implementación de ROOM

---
+++ app/src/main/java/pe/idat/apppatitas_compose/core/bd/PersonaDao.kt
@@ -0,0 +1,25 @@
+package pe.idat.apppatitas_compose.core.bd
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PersonaDao {
+
+    @Query("SELECT * FROM persona LIMIT 1")
+    fun obtenerPersona(): Flow<PersonaEntity>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertarPersona(vararg persona: PersonaEntity)
+
+    @Update
+    suspend fun actualizarPersona(vararg persona: PersonaEntity)
+
+    @Query("DELETE FROM persona")
+    suspend fun eliminarPersona()
+
+}
