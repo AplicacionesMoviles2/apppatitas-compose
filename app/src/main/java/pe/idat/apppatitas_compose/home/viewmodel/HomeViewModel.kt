2d408e7fd260d3b5cf42867244ff95a58d86fde9
AplicacionesMoviles2 <juankuga@Juans-MacBook-Pro.local>
2024-08-14 19:41:55 -0500

    Finalización del ViewModel

--- app/src/main/java/pe/idat/apppatitas_compose/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/pe/idat/apppatitas_compose/home/viewmodel/HomeViewModel.kt
@@ -6,17 +6,24 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import pe.idat.apppatitas_compose.core.bd.PersonaEntity
 import pe.idat.apppatitas_compose.home.data.network.request.VoluntarioRequest
 import pe.idat.apppatitas_compose.home.data.network.response.MascotaResponse
 import pe.idat.apppatitas_compose.home.data.network.response.VoluntarioResponse
+import pe.idat.apppatitas_compose.home.domain.ActualizarPersonaUseCase
+import pe.idat.apppatitas_compose.home.domain.EliminarPersonaUseCase
 import pe.idat.apppatitas_compose.home.domain.MascotaUseCase
+import pe.idat.apppatitas_compose.home.domain.ObtenerPersonaUseCase
 import pe.idat.apppatitas_compose.home.domain.VoluntarioUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val mascotaUseCase: MascotaUseCase,
-    private val voluntarioUseCase: VoluntarioUseCase
+    private val voluntarioUseCase: VoluntarioUseCase,
+    private val obtenerPersonaUseCase: ObtenerPersonaUseCase,
+    private val eliminarPersonaUseCase: EliminarPersonaUseCase,
+    private val actualizarPersonaUseCase: ActualizarPersonaUseCase
 ) : ViewModel() {
 
     private val _mascotaResponse = MutableLiveData<List<MascotaResponse>>()
@@ -25,6 +32,8 @@ class HomeViewModel @Inject constructor(
     val idpersona : LiveData<Int> = _idpersona
     private val _voluntarioResponse = MutableLiveData<VoluntarioResponse>()
     val voluntarioResponse : LiveData<VoluntarioResponse> = _voluntarioResponse
+    val persona : LiveData<PersonaEntity> = obtenerPersonaUseCase()
+
 
     init {
         listarMascotas()
@@ -37,12 +46,30 @@ class HomeViewModel @Inject constructor(
         }
     }
 
+    fun eliminarPersona(){
+        viewModelScope.launch {
+            eliminarPersonaUseCase()
+        }
+    }
+
     fun registrarVoluntario(){
         viewModelScope.launch {
             val response = voluntarioUseCase(
                 VoluntarioRequest(idpersona.value!!)
             )
             _voluntarioResponse.value = response
+            if(response.rpta){
+                actualizarPersonaUseCase(
+                    PersonaEntity(persona.value!!.id,
+                        persona.value!!.nombres,
+                        persona.value!!.apellidos,
+                        persona.value!!.email,
+                        persona.value!!.celular,
+                        persona.value!!.usuario,
+                        persona.value!!.password,
+                        "1")
+                )
+            }
         }
     }
 
