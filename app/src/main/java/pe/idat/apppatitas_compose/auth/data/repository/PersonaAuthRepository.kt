8001de0a8198d7a1debd4e571856d247ffc66406
AplicacionesMoviles2 <juankuga@Juans-MacBook-Pro.local>
2024-08-14 19:20:08 -0500

    Repositorios terminados

---
+++ app/src/main/java/pe/idat/apppatitas_compose/auth/data/repository/PersonaAuthRepository.kt
@@ -0,0 +1,15 @@
+package pe.idat.apppatitas_compose.auth.data.repository
+
+import pe.idat.apppatitas_compose.core.bd.PersonaDao
+import pe.idat.apppatitas_compose.core.bd.PersonaEntity
+import javax.inject.Inject
+
+
+class PersonaAuthRepository @Inject constructor(
+    private val personaDao: PersonaDao
+) {
+
+    suspend fun  insertarPersona(personaEntity: PersonaEntity){
+        personaDao.insertarPersona(personaEntity)
+    }
+}
