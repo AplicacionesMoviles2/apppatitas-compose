124745e762e9efc4e34ad579b3b60ec0ff1cab4c
AplicacionesMoviles2 <juankuga@Juans-MacBook-Pro.local>
2024-08-14 19:27:01 -0500

    UseCase Persona terminado

---
+++ app/src/main/java/pe/idat/apppatitas_compose/home/domain/ObtenerPersonaUseCase.kt
@@ -0,0 +1,14 @@
+package pe.idat.apppatitas_compose.home.domain
+
+import androidx.lifecycle.LiveData
+import pe.idat.apppatitas_compose.core.bd.PersonaEntity
+import pe.idat.apppatitas_compose.home.data.repository.PersonaHomeRepository
+import javax.inject.Inject
+
+class ObtenerPersonaUseCase @Inject constructor(
+    private val repository: PersonaHomeRepository
+) {
+    operator fun invoke(): LiveData<PersonaEntity>{
+        return repository.obtenerPersona()
+    }
+}
